24910c7bbdde09f296d42c7306586343becf906d
connor sanderford <connorsanderford@gmail.com>
2020-02-28 19:12:53 -0700

    a dummy function to make 10 records, and some starting routes.

---
+++ api/app.py
@@ -0,0 +1,63 @@
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pandas as pd
+
+from decouple import config
+
+from .dummy_functions import get_ten_tracks
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+
+    app.config['ENV'] = config('ENV')
+
+
+    # three routes
+    # /by_track_id : takes a set of track ids (including a set of one!), returns ten recommendations
+    # I'm thinking this can be used for favorites as well, if "favorites" just ends up being a set of track ids?
+    @app.route('/by_track_id', methods=['POST'])
+    def by_track_id():
+        """takes a set of track ids (including a set of one!), returns ten recommendations."""
+
+        #content = request.get_json(force=True)
+
+        ten_track_df = get_ten_tracks()
+
+        tuples = [item for item in ten_track_df.itertuples(index=False)]
+
+        labels = ['artist_name', 'track_name', 'track_id', 'danceability_diff', 'energy_diff',
+       'loudness_diff', 'mode', 'speechiness_diff', 'acousticness_diff', 'instrumentalness_diff',
+       'liveness_diff', 'valence_diff', 'tempo_diff']
+
+        tupledicts = [dict(zip(labels,tuple)) for tuple in tuples]
+
+        return json.dumps(tupledicts)
+
+
+        
+
+    # /by_custom_input : takes a feature vector, returns ten recommendations
+    @app.route('/by_custom_input', methods=['POST'])
+    def by_custom_input():
+        """takes a set of track ids (including a set of one!), returns ten recommendations."""
+
+        #content = request.get_json(force=True)
+
+        ten_track_df = get_ten_tracks()
+
+        tuples = [item for item in ten_track_df.itertuples(index=False)]
+
+        labels = ['artist_name', 'track_name', 'track_id', 'danceability_diff', 'energy_diff',
+       'loudness_diff', 'mode', 'speechiness_diff', 'acousticness_diff', 'instrumentalness_diff',
+       'liveness_diff', 'valence_diff', 'tempo_diff']
+
+        tupledicts = [dict(zip(labels,tuple)) for tuple in tuples]
+
+        return json.dumps(tupledicts)
+
+
+
+    return app
---
+++ api/dummy_functions.py
@@ -0,0 +1,64 @@
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.oauth2 as oauth2
+
+from decouple import config
+
+import pandas as pd
+
+SPOTIFY_CLIENT_ID = config('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = config('SPOTIFY_CLIENT_SECRET')
+
+credentials = oauth2.SpotifyClientCredentials(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET)
+
+token = credentials.get_access_token()
+spotify = spotipy.Spotify(auth=token)
+
+def get_ten_tracks():
+    """gets ten tracks from the spotify API, in the format they'll be served to WEB."""
+
+    artist_name = []
+    track_name = []
+    #popularity = []
+    track_id = []
+
+    track_results = spotify.search(q='year:2018 AND tag:hipster', limit=10, offset=0, market='US')
+
+    for i, t in enumerate(track_results['tracks']['items']):
+        artist_name.append(t['artists'][0]['name'])
+        track_name.append(t['name'])
+        track_id.append(t['id'])
+        #popularity.append(t['popularity'])
+
+    df_tracks = pd.DataFrame({'artist_name':artist_name,'track_name':track_name,'track_id':track_id})
+
+    rows = []
+    batchsize = 1
+    None_counter = 0
+
+    for i in range(0,len(df_tracks['track_id']),batchsize):
+        batch = df_tracks['track_id'][i:i+batchsize]
+        feature_results = spotify.audio_features(batch)
+        for i, t in enumerate(feature_results):
+            if t == None:
+                None_counter = None_counter + 1
+            else:
+                rows.append(t)
+
+    # create the audio features data frame.
+    df_audio_features = pd.DataFrame.from_dict(rows,orient='columns')
+
+    # rename id column to merge with tracks data frame.
+    df_audio_features.rename(columns={'id': 'track_id'}, inplace=True)
+
+    # drop useless columns.
+    columns_to_drop = ['analysis_url','track_href','type','uri','key','duration_ms','time_signature']
+    df_audio_features.drop(columns_to_drop, axis=1,inplace=True)
+
+    # merge both dataframes with inner method to keep track IDs present in both data frames.
+    df = pd.merge(df_tracks,df_audio_features,on='track_id',how='inner')
+
+    return df
