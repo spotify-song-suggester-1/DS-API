3da4ebb07be3b534a16356e310e449da68ecded5
kryssyco <kjshepard70@hotmail.com>
2020-03-01 22:34:35 -0700

    Added Connors code to Flask App and updated pipfile with dependancies.

--- api-app/app.py
+++ api-app/app.py
@@ -1,8 +1,21 @@
 """  Main application for Spotify Flask App """
+
+from decouple import config
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from os import getenv
+
+from .dummy_functions import get_ten_tracks
+from .spotify_functions import get_base_song_vector
+from .prediction import make_genre_vector, get_genre, augment_song_vector
+
+import json
+import pandas as pd
+import numpy as np
+
+
 
 DB = SQLAlchemy()
 
@@ -11,11 +24,68 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///spotify_tracks.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    DB.init_app(app)
+    app.config['ENV'] = config('ENV')
+    CORS(app)
 
-    conn = sqlite3.connect('spotify_tracks.sqlite3')
+    # DB.init_app(app)
+
+    # conn = sqlite3.connect('spotify_tracks.sqlite3')
 
     @app.route("/")
     def root():
         return render_template('base.html', title='Home')
+
+    @app.route('/testpath/<track_id>')
+    def testpath(track_id):
+        """Using this to test prediction functions."""
+        vec = augment_song_vector(get_base_song_vector(track_id))
+
+        labels = list(vec.index)
+        values = list(vec.values)
+
+        output = dict(zip(labels,values))
+        return output
+
+    # three routes
+    # /by_track_id : takes a set of track ids (including a set of one!), returns ten recommendations
+
+    # I'm thinking this can be used for favorites as well, if "favorites" just ends up being a set of
+    # track ids?
+
+    @app.route('/by_track_id', methods=['POST'])
+    def by_track_id():
+        """takes a set of track ids (including a set of one!), returns ten recommendations."""
+
+        #content = request.get_json(force=True)
+
+        ten_track_df = get_ten_tracks()
+
+        tuples = [item for item in ten_track_df.itertuples(index=False)]
+
+        labels = ['artist_name', 'track_name', 'track_id', 'genre', 'danceability_diff', 'energy_diff',
+       'loudness_diff', 'mode', 'speechiness_diff', 'acousticness_diff', 'instrumentalness_diff',
+       'liveness_diff', 'valence_diff', 'tempo_diff']
+
+        tupledicts = [dict(zip(labels,tuple)) for tuple in tuples]
+
+        return json.dumps(tupledicts)
+
+        # /by_custom_input : takes a feature vector, returns ten recommendations
+    @app.route('/by_custom_input', methods=['POST'])
+    def by_custom_input():
+        """takes a set of track ids (including a set of one!), returns ten recommendations."""
+
+        #content = request.get_json(force=True)
+
+        ten_track_df = get_ten_tracks()
+
+        tuples = [item for item in ten_track_df.itertuples(index=False)]
+
+        labels = ['artist_name', 'track_name', 'track_id', 'genre', 'danceability_diff', 'energy_diff',
+       'loudness_diff', 'mode', 'speechiness_diff', 'acousticness_diff', 'instrumentalness_diff',
+       'liveness_diff', 'valence_diff', 'tempo_diff']
+
+        tupledicts = [dict(zip(labels,tuple)) for tuple in tuples]
+
+        return json.dumps(tupledicts)
     return app
---
+++ api-app/predictions.py
@@ -0,0 +1,53 @@
+"""Functions for genre inference using the tensorflow model, and recommendation inference using the KNN model."""
+
+import tensorflow as tf
+import pandas as pd
+import joblib
+
+from .spotify_functions import get_base_song_vector
+
+genre_inference_features = ['acousticness', 'danceability', 'duration_ms', 'energy', 'instrumentalness', 'key','liveness', 'loudness', 'mode', 'speechiness', 'tempo', 'time_signature', 'valence']
+genre_list = ['alternative', 'country', 'dance', 'folk', 'grunge', 'indie', 'jazz', 'metal', 'pop', 'punk', 'rap', 'rock']
+genre_onehot_labels = ['genre_' + x for x in genre_list]
+
+genre_NN = tf.keras.models.load_model('genre_NN')
+scaler = joblib.load('genre_NN_scaler')
+
+
+def make_genre_vector(song_vector):
+    """takes a song vector and returns the onehot genre prediction for the song."""
+
+    feature_vector = song_vector[genre_inference_features].copy().to_numpy()
+
+    scaled_vector = scaler.transform(feature_vector.reshape(1,-1))
+
+    genre_vector = genre_NN.predict(scaled_vector)
+
+    return genre_vector
+
+
+def get_genre(genre_vector):
+    """takes a genre vector and returns the appropriate genre as a string."""
+    vector_list = genre_vector.tolist()[0]
+
+    best3_tuples =  sorted(zip(vector_list, genre_list), reverse=True)[:3]
+    best3_genres = [x[1] for x in best3_tuples]
+
+    return best3_genres
+
+
+def augment_song_vector(song_vector):
+    """takes a song vector and adds a genre string and onehot genre features."""
+
+    song_vector_output = song_vector.copy()
+    genre_vector = make_genre_vector(song_vector)
+
+    song_vector_output['genre'] = get_genre(genre_vector)
+
+    genre_dict = dict(zip(genre_onehot_labels,genre_vector.tolist()[0]))
+
+    genre_series = pd.Series(genre_dict)
+
+    song_vector_output = pd.concat([song_vector_output, genre_series])
+
+    return song_vector_output
